0e439eb9416262413b73d9b1eb6f962baffd390b
vishnu7080 <vishnuda99@gmail.com>
2026-01-21 23:13:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "TechM","Shrivalli","cts sample","FirstWeatherdata","Mild", "Warm", "Balmy", "Hot", "Sweltering","paaru", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool","vish", "TechM","Shrivalli","cts sample","FirstWeatherdata","Mild", "Warm", "Balmy", "Hot", "Sweltering","paaru", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
